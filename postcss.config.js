04976001261b031ee6c2e4952a413b9f4b240d8d
ultimatet <trungdang288@gmail.com>
2025-07-09 19:22:33 +1000

    inital commit

---
+++ postcss.config.js
@@ -0,0 +1,8 @@
+export default {
+    plugins: {
+        "@tailwindcss/postcss": {},
+        autoprefixer: {},
+    },
+};
+// This is a PostCSS configuration file for an Electron app.
+// It includes Tailwind CSS and Autoprefixer as plugins to process CSS files.
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+export default {
+    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+// This is a Tailwind CSS configuration file for an Electron app.
+// It specifies the content paths to scan for class names, extends the default theme, and includes
